f4d6e400e2d8378747a966d18ad2a2cf48bedb5e
wushuang260935@qq.com <383219847@qq.com>
2015-06-23 20:29:20 +0800

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
-from views.index import index
 from flask.ext.sqlalchemy import SQLAlchemy   
-    
-app=Flask(__name__)
-app.register_blueprint(index)
+
+from views.index import index
+from views.vip import vip
  
+app=Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////home/ws/comsay.sqlite'
 db=SQLAlchemy(app)
+
+app.register_blueprint(index)
+app.register_blueprint(vip)
+app.config["SECRET_KEY"]='COMPANY_SAY'
      
 if __name__ == '__main__':
     app.debug = True
     app.run("0.0.0.0",port=80)
+
